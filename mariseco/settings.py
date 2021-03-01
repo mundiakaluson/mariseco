6e34eba6b5365c643778649fa5c3f2965476ee44
Tutoring Learners <mac@macs-iMac.local>
2021-03-01 19:37:22 +0300

    web page optimization

--- mariseco/settings.py
+++ mariseco/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '=jg-0t%69%6yhecas^@nuy50eq%j^3=(ulzuyseyt@gh88nz16'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['d6befd0cf808.ngrok.io', '127.0.0.1']
 
 
 # Application definition

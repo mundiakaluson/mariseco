c378567a4e795d26227a211e4209969c654ded05
Tutoring Learners <mundiakaluson@gmail.com>
2021-03-28 23:15:00 +0300

    some change on deployment

--- mariseco/settings.py
+++ mariseco/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import os
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,10 +125,4 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_DIR, 'staticfiles'),
 )
 
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mundiakaluson@gmail.com'
-EMAIL_HOST_PASSWORD = '@DennisOtieno2020'
+django_heroku.settings(locals())
